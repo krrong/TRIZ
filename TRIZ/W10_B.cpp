333189b4a2842639f683a7835bb867f8be2b7345
krrong <krrong>
2021-11-17 14:06:54 +0900

    10주차 B번 마트료시카

--- TRIZ/W10_B.cpp
+++ TRIZ/W10_B.cpp
@@ -1,41 +1,41 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//using namespace std;
-//
-//int length[501];
-//int arr[501];
-//
-//int main() {
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-//	cout.tie(0);
-//
-//	int t;
-//	cin >> t;
-//	while (t--) {
-//		int n;
-//		cin >> n;
-//		for (int i = 0; i < n; i++) {
-//			cin >> arr[i];
-//		}
-//
-//		int answer = 0;
-//		for (int k = 0; k < n; k++) {
-//			length[k] = 1;
-//			for (int i = 0; i < k; i++) {
-//				if (arr[i] < arr[k]) {
-//					length[k] = max(length[k], length[i] + 1);
-//				}
-//			}
-//			if (length[k] > answer) {
-//				answer = length[k];
-//			}
-//		}
-//
-//		cout << answer << '\n';
-//	}
-//
-//
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int length[501];
+int arr[501];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			cin >> arr[i];
+		}
+
+		int answer = 0;
+		for (int k = 0; k < n; k++) {
+			length[k] = 1;
+			for (int i = 0; i < k; i++) {
+				if (arr[i] < arr[k]) {
+					length[k] = max(length[k], length[i] + 1);
+				}
+			}
+			if (length[k] > answer) {
+				answer = length[k];
+			}
+		}
+
+		cout << answer << '\n';
+	}
+
+
+	return 0;
+}
