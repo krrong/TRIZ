f8024b2ead73e39f939b97c0637f404d071888b5
krrong <krrong>
2021-12-03 15:30:29 +0900

    7주차 B번 햄버거 가게

--- TRIZ/W7_B.cpp
+++ TRIZ/W7_B.cpp
@@ -2,26 +2,31 @@
 #include <queue>
 using namespace std;
 
+int t, m, n, k;
+
+// max pq위한 comp구조체
 struct comp {
-	bool operator()(pair<int, pair<int, int>> a, pair<int, pair<int, int>> b) {
-		if (a.first > b.first) {
+	bool operator()(pair<int, int> a, pair<int, int> b) {
+		// 거리가 같으면 x좌표가 작은 것이 더 가까운 위치
+		if ((a.first * a.first + a.second * a.second) == (b.first * b.first + b.second * b.second)) {
+			if (a.first == b.first) {
+				if (a.second < b.second) {
+					return true;
+				}
+			}
+			else if (a.first < b.first) {
+				return true;
+			}
+		}
+		else if ((a.first * a.first + a.second * a.second) < (b.first * b.first + b.second * b.second)) {
 			return true;
 		}
-		else if (a.first == b.first) {
-			if (a.second.first == b.second.first) {
-				return a.second.second > b.second.second;
-			}
-			return a.second.first > b.second.first;
-		}
-		else {
-			return false;
-		}
+		return false;
 	}
 };
 
-int n, m, k, t, x, y;
+// max pq
 
-// 거리는 x^2 + y^2으로 저장
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -29,49 +34,46 @@ int main() {
 
 	cin >> t;
 	while (t--) {
-		priority_queue < pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, comp> pq;	// (거리, (x, y)) 순으로 저장
-		queue <pair<int, pair<int, int>>> q;
+		priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq;
 
 		cin >> m >> n >> k;
-		for (int i = 0; i < m; i++) {
+
+		// 기존에 존재하는 햄버거 집
+		for (int i = 0; i < m + n; i++) {
+			int x, y;
 			cin >> x >> y;
-			int dis = x * x + y * y;
-			pq.push(make_pair(dis, make_pair(x, y)));
-		}
 
-		// 가까운 k개만 남기기
-		for (int i = 0; i < k; i++) {
-			q.push(pq.top());
-			pq.pop();
-		}
-
-		// pq 비우기
-		while (!pq.empty()) {
-			pq.pop();
-		}
-
-		// pq 다시 채우기
-		for (int i = 0; i < k; i++) {
-			pq.push(q.front());
-			q.pop();
-		}
-
-		for (int i = 0; i < n; i++) {
-			cin >> x >> y;
-			int dis = x * x + y * y;
-			pair<int, pair<int, int>> tmp = make_pair(dis, make_pair(x, y));
-			pq.push(tmp);
-
-			int a = k - 1;
-			while (a--) {
-				q.push(pq.top());
-				pq.pop();
+			// pq의 사이즈가 k보다 작으면 일단 삽입
+			if (pq.size() < k) {
+				pq.push(make_pair(x, y));
 			}
-			cout << pq.top().second.first << " " << pq.top().second.second << '\n';
+			// pq의 사이즈와 k가 같으면
+			else if (pq.size() == k) {
+				pair<int, int> top = pq.top();			// top
+				pair<int, int> point = make_pair(x, y);	// 넣을 좌표
 
-			while (!q.empty()) {
-				pq.push(q.front());
-				q.pop();
+				// 새로 넣는 좌표와 pq의 top과 비교해서 top보다 작으면 top을 삭제하고 pq에 삽입한다.
+				if ((top.first * top.first + top.second * top.second) == (point.first * point.first + point.second * point.second)) {
+					if (top.first == point.first) {
+						if (top.second > point.second) {
+							pq.pop();
+							pq.push(point);
+						}
+					}
+					else if (top.first > point.first) {
+						pq.pop();
+						pq.push(point);
+					}
+				}
+				else if ((top.first * top.first + top.second * top.second) > (point.first * point.first + point.second * point.second)) {
+					pq.pop();
+					pq.push(point);
+				}
+			}
+
+			if (i >= m) {
+				cout << pq.top().first << ' ' << pq.top().second << '\n';
+
 			}
 		}
 	}
